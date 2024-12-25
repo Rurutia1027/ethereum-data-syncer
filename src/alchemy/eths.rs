1783c3b5d2338d0072345c5152b14db842128457
Rurutia1027 <rurutia1027@gmail.com>
2024-12-25 15:23:14 +0800

    add comments for eths.rs

--- src/alchemy/eths.rs
+++ src/alchemy/eths.rs
@@ -1,3 +1,23 @@
+/**
+* We have designed this architecture with the primary goal of maintaining consistency in our underlying data models,
+* regardless of the platform or method used to fetch Ethereum-associated datasets. This approach ensures that,
+* not matter which API service provider or data source we integrate -- whether it's through Alchemy, MetaMask, or a future SDK-based solution --
+* the underlying data models will remain uniform across all platforms.
+* The key reason for this design is to decouple the fetching mechanism from the data model.
+* By doing so, we can easily switch among multiple platforms, data sources, or methodologies without incurring significant modification costs
+* or affecting the integrity of our core data structures(models).
+* This becomes especially important when external platforms or API keys are deprecated,
+* changed to a paid model, or otherwise modified. In such cases, we can seamlessly switch to another platform with minimal effort,
+* and without impacting the underlying data model or existing functionality.
+
+* For instance, if a third-party provider decides to discontinue support or introduce new pricing tiers,
+* we can switch to another provider without altering the data models or API methods used by the rest of the application.
+* The same applies to changes in this endpoints or response formats at the lower level, Our data models will remain unchanged,
+* ensuring that the upper layers of our application (whether that be other APIs, microservices, or client-side code) are not impacted by these shifts.
+* This deisng pattern provides two major benefits:
+* Minmal Modification Costs
+* Seamless Integeration
+*/
 use super::BlockHash;
 use super::BlockNumber;
 use serde::{Deserialize, Serialize};
